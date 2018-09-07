cdc9aff2a91b2dafdb350c53b13b6e636512bb4e
Edgar <e_berber@u.pacific.edu>
2018-09-07 11:56:19 -0700

    finished first data struct

--- DataStruct/DataStruct.py
+++ DataStruct/DataStruct.py
@@ -1,1 +1,15 @@
-print("hello  world")
+import time
+
+print("Opening populations.csv...")
+print("Following is read in file using hash tables (Dictionaries in python).")
+words = []
+Dict = {}
+start =  time.time()
+with open('populations.csv', 'r') as myself:
+    for word in myself:
+        words = word.split(",")
+        Dict[words[0]] = words[1]
+for k, v in Dict.items():
+    print(k, ":", v)
+end = time.time()
+print("Execution time in seconds to read and map values: ", end - start)
