e689a9097236711dbed1ea90145d7817a46a3168
Edgar <e_berber@u.pacific.edu>
2018-09-07 12:13:00 -0700

    mapping keys to values in 1st data struct

--- DataStruct/DataStruct.py
+++ DataStruct/DataStruct.py
@@ -4,12 +4,20 @@ print("Opening populations.csv...")
 print("Following is read in file using hash tables (Dictionaries in python).")
 words = []
 Dict = {}
-start =  time.time()
+start = time.time()
+count = 0
 with open('populations.csv', 'r') as myself:
     for word in myself:
         words = word.split(",")
         Dict[words[0]] = words[1]
-for k, v in Dict.items():
-    print(k, ":", v)
+        count += 1
+        #print("Counter for pops.csv: ", count)
+with open('queries.txt', 'r') as mapping:
+    for store in mapping:
+        print(store)
+        # for k, v in Dict.items():
+        #     print(store, k)
+        #     if store == k:
+        #         print(k, ":", v)
 end = time.time()
 print("Execution time in seconds to read and map values: ", end - start)
