7f9626ec82cf945403cc9cc16aa94f4c4e478503
Edgar <e_berber@u.pacific.edu>
2018-09-09 13:02:50 -0700

    done comparing

--- DataStruct/DataStruct.py
+++ DataStruct/DataStruct.py
@@ -1,34 +1,52 @@
 import time
 
+
 print("Opening populations.csv...")
 print("Following is read in file using dictionaries")
 words = []
 Dict = {}
 count = 0
+l1 = []
+l2 = []
+l3 = []
 with open('populations.csv', 'r') as myself:
     for word in myself:
         words = word.split(",")
         Dict[words[0]] = words[1]
+        l1.append(words[0])
+        l3.append(words[1])
         count += 1
+l3 = [i.strip('\n') for i in l3]
 print("Reading in queries.txt...")
 lines = [line.rstrip('\n') for line in open('queries.txt')]
-# print("Starting timer")
-# start = time.time()
-# for x in lines:
-#     if x in Dict.keys():
-#         print(x, ":", Dict[x])
-# end = time.time()
-# print("Execution time in seconds to read and map values: ", end - start)
-print("Starting second data structure: sets")
-ds2 = set([])
-with open('populations.csv', 'r') as myself:
-    for word in myself:
-        ds2 = word.split(",")
-with open('queries.txt', 'r') as query:
-    for look in query:
-        print(ds2[0])
-        if look in ds2[0]:
-            print(look, ":", ds2[1])
+print(type(lines))
+print("Starting timer")
+start = time.time()
+for x in lines:
+    if x in Dict.keys():
+        print(x, ":", Dict[x])
+end = time.time()
+final = end - start
+print("Execution time in seconds to read and map values: ", final)
+
+
+print("Starting second data structure: stack")
+q = 0
+y = 0
+print("Starting timer")
+start = time.time()
+while q < len(lines):
+    same = lines[q]
+    while y < len(l3):
+        if same == l1[y]:
+            print(same, ":", l3[y])
+        y += 1
+    y = 0
+    q += 1
+end = time.time()
+print("First data struct in seconds: ", final, "Second data struct in seconds: ", end - start)
+
+
 
 
 
