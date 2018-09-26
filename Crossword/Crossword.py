93e4aad7000bb468a8bf7bbca639656c4fe77b02
Edgar <e_berber@u.pacific.edu>
2018-09-25 23:11:59 -0700

    identifying children ids and len recursively

--- Crossword/Crossword.py
+++ Crossword/Crossword.py
@@ -1,6 +1,40 @@
 import numpy
+#def findChild(x,y,boolean):
+def find_star_one(board):
+    payload = []
+    isHorz = False
+    word = 'word'
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == '*':
+                if((j + 1) < len(board[i]) and board[i][j+1] == '*'): #not out of bounds
+                    payload.append(i)
+                    payload.append(j)
+                    payload.append(True)
+                    return payload
 
-file = open('puzzle1.txt')
+def traverse(x, y , bool, board):
+    arr = []
+    start = 0
+    if bool == True: #horizontal word
+        for i in range(x,len(board)):
+            for j in range(y, len(board[i])):
+                if board[i - 1][j] > x and board[i][j] == '*':
+                    k = i
+                    while k > x : #traverse up within range
+                        k = k - 1
+
+    #else:
+        # for i in range(x,len(board)):
+        #     for j in range(y, len(board[i])):
+
+
+#MAIN
+name = input("Enter txt file name only:  ")
+try:
+    file = open(name + '.txt')
+except:
+    print("Not a valid file")
 data = file.readlines()
 col = 0
 temp = 0
@@ -12,46 +46,10 @@ for i in range(len(data)):
     else: temp += 1
 board = data[0:col]
 data = data[col:]
-print(board, '\n')
-temp = ''
-count = 0
-key = 0
-value = 0
-hash = {}
-hash2 = {}
-temp1 = 0
-temp2 = 0
-for x in data:
-#    print(x)
-    l = len(x)
-    t = str(x)
-    hash2[t] = l
-
-#print(hash2)
-
 for i in range(len(board)):
-    for j in range(len(board[i])):
-        #print(board[j] , end = '\n')
-        print(board[i][j])
-        if board[i][j] == '*': #u see potential start of word
-            count = 1 #start counter
-            temp1 = j #track start
-            q = 1
-            print("found star")
-            while board[i][j + q] == '*': #while you keep seeing stars
-                count +=1 #keep counter going to track length
-                temp2 = count #track end
-                q += 1
-            if count == 1: #no other stars found = no word,  reset
-                #print("reset")
-                count = 0
-            else: # found word
-                key += 1
-                hash[count] = key #track length and how many words have same length
-                for key in hash:
-                    if value in hash2.values(): #grab a word that matches that length
-                        str = list(hash2.keys())[list(hash2.values()).index(key)]
-                        print("here")
-                        print(str)
-        #print(j)
+    print(board[i])
+star_one = find_star_one(board)
+print(star_one)
+traverse(star_one[0], star_one[1],star_one[2], board)
+
 
