a7162caab2072cab4aabd3e21dfe818c4eb08066
Edgar <e_berber@u.pacific.edu>
2018-09-30 21:59:24 -0700

    done brute force

--- Crossword/Crossword.py
+++ Crossword/Crossword.py
@@ -1,32 +1,44 @@
 import sys
+import time
 sys.setrecursionlimit(10000)
 #GLOBAL
 node_id = 0
+
 def horzBoard(slotx, board, word):
     for i in range(len(board)):
         for j in range(len(board[i])):
-            if i == slot['xCord'] and j == slot['yCordStart']:
+            if i == slotx['xCord'] and j == slotx['yCordStart']:
                 x = 0
-                while j < slot['yCordEnd']:
+                while j <= slotx['yCordEnd']:
                     board[i][j] = word[x]
                     x += 1
                     j += 1
                 break
-    for i in range(len(board)):
-        print(board[i])
     return board
-
 
 def vertBoard(sloty, board, word):
     for j in range(len(board[0])):
         for i in range(len(board)):
-            if j == slot['yCord'] and i == slot['xCordStart']:
+            if j == sloty['yCord'] and i == sloty['xCordStart']:
                 x = 0
-                while i < slot['xCordEnd']:
+                while i <= sloty['xCordEnd']:
                     board[i][j] = word[x]
                     x += 1
                     i += 1
                 break
+    return board
+
+
+def fillBoard(slot, board, word):
+    if word == None:
+        print('Not possible')
+        return
+
+    for s in range(len(slot)):
+        if slot[s]['direction'] == 'h':
+            horzBoard(slot[s], board, word[s])
+        else:
+            vertBoard(slot[s],board,word[s])
     for i in range(len(board)):
         print(board[i])
     return board
@@ -34,32 +46,20 @@ def vertBoard(sloty, board, word):
 def solve(slot, relation, words, family, board):
     #one permuatation is possible
     #if first word fits, collision pts match move on
-    for f in range(len(relation)):
-        for s in range(len(slot)):
-            if f['P'] == slot[s]['ID']:
-                for w in range(len(words)):
-                    if len(words[w]) == s['Length']:
-                        #now does child fit?
-                        for sl in range(len(slot)):
-                            if f['C'] == slot[sl]['ID']:
-                                for wl in range(len(words)):
-                                    if len(words[wl]) == sl['Length']:
-                                        #check offset
-                                        if words[w][f['PC']] == words[wl][f['CC']]:
-                                            #add word to board, first parent
-                                            if 'xCord' in slot[s]:
-                                                return horzBoard(slot[s], board, words[w])
-                                            elif 'yCord' in slot[s]:
-                                                return vertBoard(slot[s], board, words[w])
-                                            #add child
-                                            if 'xCord' in slot[sl]:
-                                                return horzBoard(slot[sl], board, words[wl])
-                                            elif 'yCord' in slot[sl]:
-                                                return vertBoard(slot[sl], board, words[wl])
-                                        else: return #check another loop
-
-
-
+    #print(words)
+    for i in range(len(words)):
+        if len(words[i]) != slot[i]['Length']:
+            return
+    for c in relation:
+        parentID = c['P']
+        parent = words[parentID]
+        childID = c['C']
+        child = words[childID]
+        parenIndex = c['PC']
+        childIndex = c['CC']
+        if parent[parenIndex] != child[childIndex]:
+            return
+    return words
 
 def permutations(slot, relation, words, family):
     prev_word = ''
@@ -68,15 +68,12 @@ def permutations(slot, relation, words, family):
     if len(words) == 1:
         return [words]
     l = []  # empty list that will store current permutation
-
     # Iterate the input(lst) and calculate the permutation
     for i in range(len(words)):
         m = words[i]
-
         # Extract lst[i] or m from the list.  remLst is
         # remaining list
         remLst = words[:i] + words[i + 1:]
-
         # Generating all permutations where m is first
         # element
         for p in permutations(slot, relation, remLst, family):
@@ -95,7 +92,6 @@ def collision(arr, arr2):
     family = []
     t = len(arr) + len(arr2)
     graph = [[False for i in range(t)] for j in range(t)]
-    print(graph)
 
     for vert in arr2:
         for horz in arr:#x in range of vert and y in range of horz
@@ -151,48 +147,50 @@ def traverse(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             start = 0 #reset the start place
-            if board[i][j] == '*':
+            if board[i][j] == '*' :
                 length += 1
-            elif board[i][j] == '.':
+            if j ==  len(board[0]) - 1 or board[i][j + 1] == '.' :
                 if length > 1:
-                    start = j - length
-                    end = j -1
+                    start = j - length + 1
+                    end = j
                     dicHorz['xCord'] = i
                     dicHorz['yCordStart'] = start
                     dicHorz['yCordEnd'] = end
                     dicHorz['ID'] = node_id
-                    dicHorz['Length'] = end - start
+                    dicHorz['Length'] = (end - start) + 1
+                    dicHorz['direction'] = 'h'
                     arrHorz.append(dicHorz.copy()) #copy data into array
                     node_id += 1
                 length = 0
-
+    length = 0
 #vertical word
     for j in range(len(board[0])):
         for i in range(len(board)):
             start = 0 #reset the start place
             if board[i][j] == '*':
                 length += 1
-            elif board[i][j] == '.':
+            if i == len(board) -1 or board[i + 1][j] == '.' :
                 if length > 1:
-                    start = i - length
-                    end = i -1
+                    start = i - length + 1
+                    end = i
                     dicVert['yCord'] = j
                     dicVert['xCordStart'] = start
                     dicVert['xCordEnd'] = end
                     dicVert['ID'] = node_id
                     node_id += 1
-                    dicVert['Length'] = end - start
+                    dicVert['Length'] = (end - start) + 1
+                    dicVert['direction'] = 'v'
                     arrVert.append(dicVert.copy()) #copy data into array
                 length = 0
     return [arrHorz, arrVert];
 
 #MAIN
-# name = input("Enter txt file name only:  ")
-# try:
-#     file = open(name + '.txt')
-# except:
-#     print("Not a valid file")
-file = open('puzzle1.txt')
+name = input("Enter txt file name only:  ")
+try:
+    file = open(name + '.txt')
+except:
+    print("Not a valid file")
+#file = open('puzzle1.txt')
 data = file.readlines()
 col = 0
 temp = 0
@@ -204,35 +202,31 @@ for i in range(len(data)):
     else: temp += 1
 board = data[0:col]
 data = data[col:] #words
-for i in range(len(board)):
-    print(board[i])
+print('Words')
 for d in range(len(data)):
     print(data[d], 'Len: ', len(data[d]))
 val1, val2 = traverse(board)
+print('slots')
 for s in val1:
     print(s)
 for s in val2:
     print(s)
-pairs,slot, family, graph, together = collision(val1,val2)
+pairs,point, family, graph, together = collision(val1,val2)
 collide = {}
-print(type(slot))
-for x in pairs:
-     print('Collision ID pairs: ', x)
-for y in slot:
-    print("Offset point: ", y)
-print('Relation')
-for z in family:
-    print(z)
-print('Together')
-for i in together:
-    print(i)
 #call to perms is a loop
 x = 0
-for p in permutations(slot, together, data, family):
-    print(p)
-    print('\n DONE  \n', x )
-    solve(slot, together, p, family, board)
+#put all slots together
+for v in val2:
+    val1.append(v)
+start = time.time()
+answer = None
+for p in permutations(val1, together, data, family):
+    answer = solve(val1, together, p, family, board)
+    if answer != None:
+        break
     x += 1
-
-#solve(val1 , val2, data , colRelation, None)
-
+print('\n')
+print('Answer' , answer, '\n')
+fillBoard(val1, board, answer)
+end = time.time()
+print('Time: ', end - start)
