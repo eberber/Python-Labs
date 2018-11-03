e7b975ddd5a294cdcfcbb77bc4e2d41654597099
Edgar <e_berber@u.pacific.edu>
2018-11-02 17:29:02 -0700

    done

--- fuzzySearch/fuzzySearch.py
+++ fuzzySearch/fuzzySearch.py
@@ -1,3 +1,26 @@
+def shiftTable(pattern):
+    table = []
+    map = {}
+    alphabet = []
+    patt = []
+    # m is len of pattern
+    for i in range(len(pattern)):
+        patt.append(pattern)
+        if pattern[i] not in map.keys():
+            map.update({pattern[i] : 1})
+    for key in map:
+        alphabet.append(key)
+    print(alphabet)
+    for i in range(len(pattern) - 1):
+        table[i] = len(pattern)
+        for j in range(len(pattern) - 2):
+            table[patt[j]] = len(pattern) - 1 - j
+    print(pattern)
+    return table
+
 with open('dictionary.txt') as file:
     data = file.read()
 print(data)
+phrase = input("Enter a phrase to search for: ")
+print("Word is : ", phrase, "\n length: ", len(phrase))
+shiftTable(phrase)
