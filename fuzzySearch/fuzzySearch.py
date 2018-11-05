ce41617f36c820ffb5db8fe89aa062f63e52cb07
Edgar <e_berber@u.pacific.edu>
2018-11-05 12:11:33 -0800

    implemented basic search

--- fuzzySearch/fuzzySearch.py
+++ fuzzySearch/fuzzySearch.py
@@ -1,26 +1,44 @@
-def shiftTable(pattern):
-    table = []
-    map = {}
-    alphabet = []
-    patt = []
-    # m is len of pattern
-    for i in range(len(pattern)):
-        patt.append(pattern)
-        if pattern[i] not in map.keys():
-            map.update({pattern[i] : 1})
-    for key in map:
-        alphabet.append(key)
-    print(alphabet)
-    for i in range(len(pattern) - 1):
-        table[i] = len(pattern)
-        for j in range(len(pattern) - 2):
-            table[patt[j]] = len(pattern) - 1 - j
-    print(pattern)
-    return table
+import re
 
-with open('dictionary.txt') as file:
+def match(my_word, data):
+    flag = 0
+    payload = []
+    string1 = ""
+    counter = 0
+    num_matches = 0
+    #if a single letter does not match, thats one. on a second dont append
+    for i in range(len(data)):
+        print(data[i])
+        if i >= len(my_word): #make sure word is of same length to compare
+            print("XXXXXXXXX")
+            for j in range(len(my_word)):
+                string1 += data[(i - len(my_word))+j]
+                if my_word[j] != data[(i-len(my_word)) + j]:
+                    flag +=1
+                    if flag == 2:
+                        break
+            if flag != 2:
+                num_matches+=1
+                payload.append(string1)
+                if string1 == my_word:
+                    break
+            string1 = ""
+            flag = 0
+            counter = 0
+        counter +=1
+    return payload, num_matches
+
+
+with open('small.txt') as file:
     data = file.read()
-print(data)
+data = re.findall(r"[\S]",data)
 phrase = input("Enter a phrase to search for: ")
 print("Word is : ", phrase, "\n length: ", len(phrase))
-shiftTable(phrase)
+payload, num_matches = match(phrase, data)
+for i in payload:
+    if i == phrase:
+        print(i)
+        print("Exact match")
+        exit(0)
+print("result",payload)
+print('Matches',num_matches)
