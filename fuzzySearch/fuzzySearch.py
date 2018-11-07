11de4470d20cca4c4742e8bf8b6aceb2cac64611
Edgar <e_berber@u.pacific.edu>
2018-11-06 20:55:48 -0800

    fixed some bugssss

--- fuzzySearch/fuzzySearch.py
+++ fuzzySearch/fuzzySearch.py
@@ -1,31 +1,5 @@
-import re
+import time
 import numpy as np
-
-def match(my_word, data):
-    flag = 0
-    payload = []
-    string1 = ""
-    counter = 0
-    num_matches = 0
-    #if a single letter does not match, thats one. on a second dont append
-    for i in range(len(data)):
-        if i >= len(my_word): #make sure word is of same length to compare
-            for j in range(len(my_word)):
-                string1 += data[(i - len(my_word))+j]
-                if my_word[j] != data[(i-len(my_word)) + j]:
-                    flag +=1
-                    if flag == 2:
-                        break
-            if flag != 2:
-                num_matches+=1
-                payload.append(string1)
-                if string1 == my_word:
-                    break
-            string1 = ""
-            flag = 0
-            counter = 0
-        counter +=1
-    return payload, num_matches
 
 def levenshtein(seq1, seq2):
     size_x = len(seq1) + 1
@@ -73,11 +47,12 @@ def search(my_word, data):
                     num_matches += 1
                     payload.append(string1)
                 elif score == 2:# may be a swap?
-                    for x in range(len(string1)):
+                    for x in range(len(my_word)):
                         if my_word[x] not in dict.keys():
                             dict[my_word[x]] = 1
                         else:
                             dict[my_word[x]] += 1
+                    for x in range(len(string1)):
                         if string1[x] not in dict.keys():
                             dict[string1[x]] = 1
                         else:
@@ -94,18 +69,21 @@ def search(my_word, data):
             string1 = ""
     return payload, num_matches
 
-
-with open('code.cpp') as file:
+with open('small.txt') as file:
     data = file.read().lower()
 print(len(data))
 phrase = input("Enter a phrase to search for: \n")
 phrase = phrase.lower()
 print("\n Word is : ", phrase, "\n length: \n", len(phrase))
+start = time.time()
 payload, num_matches = search(phrase, data)
+final = time.time()
 for i in payload:
     if i == phrase:
         print(i)
         print("Exact match")
+        print("Time: ", final - start)
         exit(0)
 print("result \n", payload)
 print('Matches', num_matches)
+print("Time: ", final - start)
