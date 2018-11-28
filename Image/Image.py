07cbf448d060424cb25f9ce31c332a79f95940b7
Edgar <e_berber@u.pacific.edu>
2018-11-27 22:42:28 -0800

    obj vs str

--- Image/Image.py
+++ Image/Image.py
@@ -2,13 +2,20 @@ from PIL import Image
 import time
 import queue
 
-class HuffmanNode(object):
-    def __init__(self, left=None, right=None, root=None):
+
+class Hman(object):
+    def __init__(self, left=None, right=None):
         self.left = left
         self.right = right
-        self.root = root     # Why?  Not needed for anything.
+
+    def __lt__(self, other):
+        if type(self) == str:
+            return self
+        return 0
+
     def children(self):
-        return((self.left, self.right))
+        return self.left, self.right
+
 
 def create_freq_table():
     img = Image.open("powercat.bmp")
@@ -35,55 +42,68 @@ def create_freq_table():
             dict[b] += 1
         else:
             dict[b] = 1
-        count_bits+=1
+        count_bits += 1
     count_bits = count_bits * 24
     print("Required bits for fixed length: ", count_bits)
     for i in dict.values():
         total += i
     temp = 0
+    tuple = ()
+    list = []
     for i, j in dict.items():
         dict[i] = j/total
-        print(i, dict[i])
         temp += dict[i]
+        tuple = (dict[i], str(i))
+        list.append(tuple)
+    for i in range(len(dict)):
+        for j in range(i+1, len(dict)):
+            if dict[i] == dict[j]:
+                print("EQUAL", dict[i], dict[j] )
+    print(list)
     print("TOTAL: ", total, "TEMP: ", temp)
-    return dict
-
-freq = {}
-freq = create_freq_table()
+    return list
 
 def create_tree(frequencies):
     p = queue.PriorityQueue()
+    print(type(frequencies))
     for value in frequencies:    # 1. Create a leaf node for each symbol
-        p.put(value)             #    and add it to the priority queue
+        p.put(value)             #and add it to the priority queue
     while p.qsize() > 1:         # 2. While there is more than one node
         l, r = p.get(), p.get()  # 2a. remove two highest nodes
-        node = HuffmanNode(l, r) # 2b. create internal node with children
+        node = Hman(l, r) # 2b. create internal node with children
         p.put((l[0]+r[0], node)) # 2c. add new node to queue
+        print('HERE', l,r)
     return p.get()               # 3. tree is complete - return root node
-freq = [
+
+
+freq = create_freq_table()
+
+"""freq = [
     (8.167, 'a'), (1.492, 'b'), (2.782, 'c'), (4.253, 'd'),
     (12.702, 'e'),(2.228, 'f'), (2.015, 'g'), (6.094, 'h'),
     (6.966, 'i'), (0.153, 'j'), (0.747, 'k'), (4.025, 'l'),
     (2.406, 'm'), (6.749, 'n'), (7.507, 'o'), (1.929, 'p'),
     (0.095, 'q'), (5.987, 'r'), (6.327, 's'), (9.056, 't'),
     (2.758, 'u'), (1.037, 'v'), (2.365, 'w'), (0.150, 'x'),
-    (1.974, 'y'), (0.074, 'z') ]
+    (1.974, 'y'), (0.074, 'z')]"""
+
+
 node = create_tree(freq)
-print("hehrjkhfiehraskjlf", node)
 
 # Recursively walk the tree down to the leaves,
 #   assigning a code value to each symbol
 def walk_tree(node, prefix="", code={}):
-    if isinstance(node[1].left[1], HuffmanNode):
+    if isinstance(node[1].left[1], Hman):
         walk_tree(node[1].left,prefix+"0", code)
     else:
         code[node[1].left[1]]=prefix+"0"
-    if isinstance(node[1].right[1],HuffmanNode):
+    if isinstance(node[1].right[1],Hman):
         walk_tree(node[1].right,prefix+"1", code)
     else:
         code[node[1].right[1]]=prefix+"1"
-    return(code)
+    return code
+
 
 code = walk_tree(node)
-#for i in sorted(freq, reverse=True):
- #   print(i[1], '{:6.2f}'.format(i[0]), code[i[1]])
+for i in sorted(freq, reverse=True):
+    print(i[1], '{:6.2f}'.format(i[0]), code[i[1]])
